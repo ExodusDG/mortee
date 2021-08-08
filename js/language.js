af1e37e8ec7941213421e86b50a0b5c505c7571e
ExodusDG <sanistray@gmail.com>
2021-08-08 19:11:18 +0300

    Update language.js

--- js/language.js
+++ js/language.js
@@ -1,6 +1,21 @@
-var lang = navigator.languages
-console.log(navigator.languages)
-if (lang.indexOf('ru')) {
+function browserLocale() {
+    var lang
+
+    if (navigator.languages && navigator.languages.length) {
+        // latest versions of Chrome and Firefox set this correctly
+        lang = navigator.languages[0]
+    } else if (navigator.userLanguage) {
+        // IE only
+        lang = navigator.userLanguage
+    } else {
+        // latest versions of Chrome, Firefox, and Safari set this correctly
+        lang = navigator.language
+    }
+
+    return lang
+}
+
+if (browserLocale().indexOf('ru') !== 1) {
     //
 } else {
     engLang()
